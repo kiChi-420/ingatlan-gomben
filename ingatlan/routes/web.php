006043c0ad647041a1941948eeb3cd3f253de755
ben <gomben397@hengersor.hu>
2024-09-23 10:17:43 +0200

    navbar hozzáadása
    A Tailwind css-ben reszponzív az oldal

--- ingatlan/routes/web.php
+++ ingatlan/routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function() {
+Route::get('/user', function() {
     return view('user');
 });
-Route::get('/', function() {
+Route::get('/login', function() {
     return view('login');
 });
